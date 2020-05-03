e287c69a00e0785d81e20580c70104bf58211523
Andrew <24258342+acodeguy@users.noreply.github.com>
2020-05-03 09:55:47 +0100

    Add readme, .gitignore and delete header comments

--- WeatherMVVM/AppDelegate.swift
+++ WeatherMVVM/AppDelegate.swift
@@ -1,17 +1,7 @@
-//
-//  AppDelegate.swift
-//  WeatherMVVM
-//
-//  Created by Andrew on 03/05/2020.
-//  Copyright © 2020 Andrew. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
